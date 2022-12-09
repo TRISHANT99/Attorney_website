ee8e8808159b762889591c7693a99209c2597f5d
kunallkush <kunalkuswah7480.com>
2022-12-09 16:26:17 +0530

    navbar-creation

--- website_app/src/components/Navbar.js
+++ website_app/src/components/Navbar.js
@@ -1,59 +1,76 @@
 import React from "react";
 import "../css/Navbar.css";
-
 export default function Navbar() {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            <img
-              style={{ height: "35px", width: "35px", borderRadius: "50px" }}
-              src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0/1519855918965?e=2147483647&v=beta&t=J3kUMZwIphc90TFKH5oOO9Sa9K59fimgJf-s_okU3zs"
-              alt="pic"
-            />{" "}
-            Person_Name
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
+      <nav className="navbar navbar-expand-lg navbar-dark ">
+        <div className="container ">
+          <div className="col-md-4 ">
+            <a className="navbar-brand" href="/">
+              <img
+                className=""
+                src="logo\7459344-removebg.png"
+                style={{ height: "150px", width: "180px" }}
+                alt=""
+              />
+            </a>
+          </div>
+
+          <div
+            className=" collapse navbar-collapse col-md-8"
+            id="navbarSupportedContent"
           >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+              <li className="nav-item me-3">
+                <a
+                  className=" nav-link active hover-underline-animation "
+                  id="menuItem"
+                  aria-current="page"
+                  href="/"
+                >
                   Home
                 </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="/">
+              <li className="nav-item me-3">
+                <a
+                  className="nav-link active hover-underline-animation"
+                  id="menuItem"
+                  href="/"
+                >
+                  Services
+                </a>
+              </li>
+              <li className="nav-item me-3">
+                <a
+                  className="nav-link active hover-underline-animation"
+                  id="menuItem"
+                  href="/"
+                >
+                  Insights
+                </a>
+              </li>
+              <li className="nav-item me-3">
+                <a
+                  className="nav-link active hover-underline-animation"
+                  id="menuItem"
+                  href="/"
+                >
+                  About Us
+                </a>
+              </li>{" "}
+              <li className="nav-item me-3">
+                <a
+                  className="nav-link active hover-underline-animation"
+                  id="menuItem"
+                  href="/"
+                >
                   Contact Us
                 </a>
               </li>
-              <li className="nav-item">
-                <a className="nav-link active" href="/">
-                  About Us
-                </a>
+              <li className="nav-item me-3">
+                <img className="mt-2" src="logo\search.svg" />
               </li>
             </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
           </div>
         </div>
       </nav>
